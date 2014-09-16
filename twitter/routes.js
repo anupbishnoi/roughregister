4011b521369fa10f831d406d3a5a0c9ef46727f9
Anup Bishnoi <pixelsallover@gmail.com>
2014-09-16 20:05:36 +0000

    now check anyone's twitter followers at /twitter/:handle

---
+++ twitter/routes.js
@@ -0,0 +1,19 @@
+var assert = require('assert'),
+    request = require('request'),
+    cheerio = require('cheerio');
+
+module.exports = function (app) {
+    app.get('/:handle', function (req, res) {
+        var handle = req.params.handle;
+        
+        assert.equal(typeof handle, 'string');
+        request('https://twitter.com/' + handle, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                res.send($('[data-nav=followers] .ProfileNav-value').text());
+            } else {
+                res.send(response.statusCode + ': ' + error);
+            }
+        });
+    });
+};
