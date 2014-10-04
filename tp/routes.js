dedc6c1a03035f41aed3330460795813bb3db16f
Anup Bishnoi <pixelsallover@gmail.com>
2014-10-04 05:08:19 +0000

    feeling loved :)

--- tp/routes.js
+++ tp/routes.js
@@ -3,10 +3,26 @@ var config = require('./twilio-config'),
     authToken = config.authToken;
 
 var client = require('twilio')(accountSid, authToken);
-client.messages.create({
-    body: "ae..",
-    to: config.to,
-    from: config.from
-}, function(err, message) {
-    process.stdout.write(message.sid);
-});
+
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        var phones = config.phones;
+        phones.forEach(function (phone, i) {
+            app.log('sending to', phone);
+            client.messages.create({
+                body: config.msg,
+                from: config.from,
+                to: phone
+            }, function(err) {
+                if (err) return;
+                app.log('sent to', phone);
+                feelingLoved();
+            });
+        
+        });
+        var total = phones.length;
+        function feelingLoved() {
+            --total || res.send('feeling loved :)');
+        }
+    });
+};
