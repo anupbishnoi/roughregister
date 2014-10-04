b9b3b35a752138fe565151a845fd53497c9d64eb
Anup Bishnoi <pixelsallover@gmail.com>
2014-10-04 04:12:12 +0000

    works!

--- routes/memory-usage.js
+++ routes/memory-usage.js
@@ -1,7 +1,6 @@
 var humanize = require('humanize');
 module.exports = function (app) {
     app.get('/memory-usage', function (req, res) {
-        ljshdfljh();
         res.send(humanize.filesize(process.memoryUsage().rss));
     });
 };
