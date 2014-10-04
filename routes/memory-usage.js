5f2cbed15d6a18f5146a1494d21b6f780196479b
Anup Bishnoi <pixelsallover@gmail.com>
2014-10-04 04:09:40 +0000

    testing resilience of redeploy script

--- routes/memory-usage.js
+++ routes/memory-usage.js
@@ -1,6 +1,7 @@
 var humanize = require('humanize');
 module.exports = function (app) {
     app.get('/memory-usage', function (req, res) {
+        throw 'lol';
         res.send(humanize.filesize(process.memoryUsage().rss));
     });
 };
