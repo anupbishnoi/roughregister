81b9a6a5d7d6d62d4a5c7acbd0b023057f2e73b7
Anup Bishnoi <pixelsallover@gmail.com>
2014-10-23 22:25:26 +0000

    ok md editor complete

--- write/routes.js
+++ write/routes.js
@@ -18,10 +18,10 @@ module.exports = function (app) {
       if (!val) return res.redirect(
         app.mountPath + '/' + name + '/edit'
       );
-      app.log('view md', val);
+      // app.log('view md', val);
       marked(val, function (err, html) {
         if (err) return res.end(err.stack);
-        app.log('html', html);
+        // app.log('html', html);
         res.send(viewTop + html + viewBottom);
       });
     });
@@ -33,17 +33,17 @@ module.exports = function (app) {
     find(name, function (err, md) {
       if (err) return res.end(err.stack);
       md = md || '';
-      app.log('edit md', md);
+      // app.log('edit md', md);
       res.send(editTop + md + editBottom);
     });
   });
   
   app.post('/:name/save', function (req, res){
     var name = req.params.name,
-      val = req.body;
+      content = req.body.content;
     app.log('save', name);
-    app.log('save md', val);
-    db.set(name, val, function () {
+    // app.log('save md', content);
+    db.set(name, content, function () {
       res.send('success');
     });
   });
@@ -75,11 +75,13 @@ function stream(file) {
 }
 
 function find(key, fn) {
+  var found;
   db.forEach(function (k, val) {
     if (key === k) {
+      found = true;
       fn(null, val);
       return false;
     }
   });
-  fn(null, undefined);
+  if (!found) fn(null, undefined);
 }
